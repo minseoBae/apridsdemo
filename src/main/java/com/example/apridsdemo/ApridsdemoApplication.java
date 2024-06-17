e2a546483bbd73843b505925cff1477c0a349eba
minseoBae <baejjae8218@gmail.com>
2024-06-17 21:18:44 +0900

    first commit

---
+++ src/main/java/com/example/apridsdemo/ApridsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apridsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApridsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApridsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apridsdemo/ApridsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apridsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApridsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
